5d3ef05037631a4fa9cf883c16e48719f6a07ecf
Ingrid Haahjem <ingridmargit@gmail.com>
2023-11-17 13:41:11 +0100

    removed ptq size check

--- src/main.cc
+++ src/main.cc
@@ -429,7 +429,7 @@ int main(int argc, char** argv)
         ooo_cpu[i]->instrs_to_speculate_this_cycle = std::min<std::size_t>(ooo_cpu[i]->FETCH_WIDTH, ooo_cpu[i]->num_empty_ftq_entries); 
       }
 
-      while (ooo_cpu[i]->fetch_stall == 1 && ooo_cpu[i]->instrs_to_speculate_this_cycle > 0 && ooo_cpu[i]->num_empty_ftq_entries > 0 && ooo_cpu[i]->PTQ.size() < MAX_PTQ_ENTRIES){
+      while (ooo_cpu[i]->fetch_stall == 1 && ooo_cpu[i]->instrs_to_speculate_this_cycle > 0 && ooo_cpu[i]->num_empty_ftq_entries > 0){
         ooo_cpu[i]->prefetch_past_mispredict();
         ooo_cpu[i]->has_speculated = 1;
       }
