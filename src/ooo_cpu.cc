b6f283bd4232c4098013838412907065e8f6ac90
Ingrid Haahjem <ingridmargit@gmail.com>
2023-11-17 13:49:31 +0100

    Moved counter back to trace funct

--- src/ooo_cpu.cc
+++ src/ooo_cpu.cc
@@ -18,7 +18,7 @@ extern uint8_t MAX_INSTR_DESTINATIONS;
 void O3_CPU::operate()
 {
   instrs_to_read_this_cycle = std::min((std::size_t)FETCH_WIDTH, IFETCH_BUFFER.size() - IFETCH_BUFFER.occupancy());
-  num_empty_ftq_entries     = IFETCH_BUFFER.size() - IFETCH_BUFFER.occupancy();
+  
 
   retire_rob();                    // retire
   complete_inflight_instruction(); // finalize execution
@@ -234,6 +234,7 @@ void O3_CPU::init_instruction(ooo_model_instr arch_instr)
           btb_input = std::make_pair(predicted_branch_target, always_taken);
         }
         
+        num_empty_ftq_entries     = IFETCH_BUFFER.size() - IFETCH_BUFFER.occupancy();
         instrs_to_speculate_this_cycle = instrs_to_read_this_cycle;
         
         fetch_stall = 1;
@@ -461,7 +462,7 @@ void O3_CPU::promote_to_decode()
       DECODE_BUFFER.push_back(IFETCH_BUFFER.front());
 
     IFETCH_BUFFER.pop_front();
-    num_empty_ftq_entries++;
+    // num_empty_ftq_entries++;
 
     available_fetch_bandwidth--;
   }
